18060b619e2cd689225439c7a971b942f778ca97
Paulus Esterhazy <pesterhazy@gmail.com>
2023-05-23 20:10:30 +0200

    WIP

--- test/bash2bb/core_test.clj
+++ test/bash2bb/core_test.clj
@@ -1,6 +1,19 @@
 (ns bash2bb.core-test
   (:require
+   [cheshire.core :as json]
    [clojure.test :refer [deftest is]]))
 
+(defn bla []
+  (let [stmts (-> (slurp "examples/simple.json")
+                  json/parse-string
+                  (get "Stmts"))]
+    (map (fn [v] (map (fn [arg]
+                        (-> arg (get "Parts") first (get "Value")))
+
+                      (-> v
+                          (get "Cmd")
+                          (get "Args"))))
+         stmts)))
+
 (deftest banana
-  (is (= 0 1)))
+  (is (= [["echo" "one"]] (bla))))
