93cafba4e83e0f557bc6fc14d53387fc79b74629
Paulus Esterhazy <pesterhazy@gmail.com>
2023-05-23 20:16:55 +0200

    WIP

--- test/bash2bb/core_test.clj
+++ test/bash2bb/core_test.clj
@@ -3,17 +3,24 @@
    [cheshire.core :as json]
    [clojure.test :refer [deftest is]]))
 
-(defn bla []
+(defn bla
+  []
   (let [stmts (-> (slurp "examples/simple.json")
                   json/parse-string
                   (get "Stmts"))]
-    (map (fn [v] (map (fn [arg]
-                        (-> arg (get "Parts") first (get "Value")))
-
-                      (-> v
-                          (get "Cmd")
-                          (get "Args"))))
-         stmts)))
+    (map
+     (fn [v]
+       (apply list
+              (into ['shell]
+                    (map (fn [arg]
+                           (-> arg
+                               (get "Parts")
+                               first ;; this is pretty iffy!
+                               (get "Value")))
+                         (-> v
+                             (get "Cmd")
+                             (get "Args"))))))
+     stmts)))
 
 (deftest banana
-  (is (= [["echo" "one"]] (bla))))
+  (is (= [(list 'shell "echo" "one")] (bla))))
