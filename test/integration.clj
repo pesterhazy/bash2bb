faf2773c3f12b61576ee94a77125429b0d20be8a
Paulus Esterhazy <pesterhazy@gmail.com>
2023-05-27 10:44:16 +0200

    WIP

--- test/integration.clj
+++ test/integration.clj
@@ -1,13 +1,16 @@
 (ns integration
-  (:require [clojure.test :refer [deftest is run-tests]]
-            [babashka.process :refer [shell]]
-            [bash2bb.core :as x]))
+  (:require
+   [babashka.fs :as fs]
+   [babashka.process :refer [shell]]
+   [bash2bb.core :as x]
+   [clojure.test :refer [deftest is run-tests]]))
 
 (deftest integration
-  (let [bash (slurp "integration/envvar")
-        bb (str (x/bash->bb bash) "\n" "nil")]
-    (is (= (:out (shell {:out :string} "bb" "-e" bb))
-           (:out (shell {:out :string} "bash" "-c" bash))))))
+  (doseq [fname (fs/glob "integration" "*.bash")]
+    (let [bash (slurp (str fname))
+          bb (str (x/bash->bb bash) "\n" "nil")]
+      (is (= (:out (shell {:out :string} "bb" "-e" bb))
+             (:out (shell {:out :string} "bash" "-c" bash)))))))
 
 (defn -main []
   (run-tests 'integration))
