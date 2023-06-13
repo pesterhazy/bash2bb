616cd4d8220e0b8edd103e2a8b7b6205c23062cd
Paulus Esterhazy <pesterhazy@gmail.com>
2023-06-13 21:27:42 +0200

    WIP

--- test/bash2bb/core_test.clj
+++ test/bash2bb/core_test.clj
@@ -22,67 +22,67 @@
   (is (= []
          (x/ast->forms (x/bash->ast "")))))
 
-(deftest echo-one
-  (is (= [(list 'shell "echo" "one")]
-         (x/ast->forms (x/bash->ast "echo one")))))
+(deftest cmd-one
+  (is (= [(list 'shell "cmd" "one")]
+         (x/ast->forms (x/bash->ast "cmd one")))))
 
-(deftest echo-two
-  (is (= [(list 'shell "echo" "one")
-          (list 'shell "echo" "two")]
-         (x/ast->forms (x/bash->ast "echo one\necho two")))))
+(deftest cmd-two
+  (is (= [(list 'shell "cmd" "one")
+          (list 'shell "cmd" "two")]
+         (x/ast->forms (x/bash->ast "cmd one\ncmd two")))))
 
-(deftest echo-double-quotes
-  (is (= [(list 'shell "echo" "a b")]
-         (x/ast->forms (x/bash->ast "echo \"a b\"")))))
+(deftest cmd-double-quotes
+  (is (= [(list 'shell "cmd" "a b")]
+         (x/ast->forms (x/bash->ast "cmd \"a b\"")))))
 
-(deftest echo-single-quotes
-  (is (= [(list 'shell "echo" "a b")]
-         (x/ast->forms (x/bash->ast "echo 'a b'")))))
+(deftest cmd-single-quotes
+  (is (= [(list 'shell "cmd" "a b")]
+         (x/ast->forms (x/bash->ast "cmd 'a b'")))))
 
-(deftest echo-pipe
-  (is (= ['(shell {:in (:out (shell {:out :string} "echo" "ab"))} "rev")]
-         (x/ast->forms (x/bash->ast "echo ab | rev")))))
+(deftest cmd-pipe
+  (is (= ['(shell {:in (:out (shell {:out :string} "cmd" "ab"))} "rev")]
+         (x/ast->forms (x/bash->ast "cmd ab | rev")))))
 
-(deftest echo-redirect-stdout
-  (is (= '[(shell {:out "stdout.txt"} "echo" "a")]
-         (x/ast->forms (x/bash->ast "echo a > stdout.txt")))))
+(deftest cmd-redirect-stdout
+  (is (= '[(shell {:out "stdout.txt"} "cmd" "a")]
+         (x/ast->forms (x/bash->ast "cmd a > stdout.txt")))))
 
-(deftest echo-redirect-stderr
-  (is (= '[(shell {:err "stderr.txt"} "echo" "a")]
-         (x/ast->forms (x/bash->ast "echo a 2> stderr.txt")))))
+(deftest cmd-redirect-stderr
+  (is (= '[(shell {:err "stderr.txt"} "cmd" "a")]
+         (x/ast->forms (x/bash->ast "cmd a 2> stderr.txt")))))
 
-(deftest echo-redirect-stdin
+(deftest cmd-redirect-stdin
   (is (= '[(shell {:in (slurp "stdin.txt")} "cat")]
          (x/ast->forms (x/bash->ast "cat < stdin.txt")))))
 
-(deftest echo-redirect-both
-  (is (= '[(shell {:out "both.txt", :err :out} "echo" "a")]
-         (x/ast->forms (x/bash->ast "echo a >& both.txt")))))
+(deftest cmd-redirect-both
+  (is (= '[(shell {:out "both.txt", :err :out} "cmd" "a")]
+         (x/ast->forms (x/bash->ast "cmd a >& both.txt")))))
 
-(deftest echo-redirect-stdout-to-fd
-  (is (= ['(shell {:out System/err} "echo" "a")]
-         (x/ast->forms (x/bash->ast "echo a >&2")))))
+(deftest cmd-redirect-stdout-to-fd
+  (is (= ['(shell {:out System/err} "cmd" "a")]
+         (x/ast->forms (x/bash->ast "cmd a >&2")))))
 
-(deftest echo-redirect-stderr-to-stdout
-  (is (= ['(shell {:err System/out} "echo" "a")]
-         (x/ast->forms (x/bash->ast "echo a 2>&1")))))
+(deftest cmd-redirect-stderr-to-stdout
+  (is (= ['(shell {:err System/out} "cmd" "a")]
+         (x/ast->forms (x/bash->ast "cmd a 2>&1")))))
 
-(deftest echo-pipe-3
-  (is (= ['(shell {:in (:out (shell {:in (:out (shell {:out :string} "echo" "ab")) :out :string} "cat"))} "rev")]
-         (x/ast->forms (x/bash->ast "echo ab | cat | rev")))))
+(deftest cmd-pipe-3
+  (is (= ['(shell {:in (:out (shell {:in (:out (shell {:out :string} "cmd" "ab")) :out :string} "cat"))} "rev")]
+         (x/ast->forms (x/bash->ast "cmd ab | cat | rev")))))
 
-(deftest echo-cmd-subst
-  (is (= '[(shell "echo" (:out (shell {:out :string} "echo" "a")))]
-         (x/ast->forms (x/bash->ast "echo $(echo a)")))))
+(deftest cmd-cmd-subst
+  (is (= '[(shell "cmd" (:out (shell {:out :string} "cmd" "a")))]
+         (x/ast->forms (x/bash->ast "cmd $(cmd a)")))))
 
-(deftest echo-cmd-subst-2-stmts
-  (is (= '[(shell "echo" (str (:out (shell {:out :string} "echo" "a"))
-                              (:out (shell {:out :string} "echo" "b"))))]
-         (x/ast->forms (x/bash->ast "echo $(echo a; echo b)")))))
+(deftest cmd-cmd-subst-2-stmts
+  (is (= '[(shell "cmd" (str (:out (shell {:out :string} "cmd" "a"))
+                             (:out (shell {:out :string} "cmd" "b"))))]
+         (x/ast->forms (x/bash->ast "cmd $(cmd a; cmd b)")))))
 
-(deftest echo-2-parts
-  (is (= '[(shell "echo" (str (:out (shell {:out :string} "echo" "a")) "="))]
-         (x/ast->forms (x/bash->ast "echo \"$(echo a)=\"")))))
+(deftest cmd-2-parts
+  (is (= '[(shell "cmd" (str (:out (shell {:out :string} "cmd" "a")) "="))]
+         (x/ast->forms (x/bash->ast "cmd \"$(cmd a)=\"")))))
 
 (deftest here-string
   (is (= '[(shell {:in "abc"} "cat")]
@@ -93,32 +93,32 @@
          (x/ast->forms (x/bash->ast "cat <<END\nhello\nworld\nEND")))))
 
 (deftest param-exp
-  (is (= '[(shell "echo" VAR)]
-         (x/ast->forms (x/bash->ast "echo $VAR")))))
+  (is (= '[(shell "cmd" VAR)]
+         (x/ast->forms (x/bash->ast "cmd $VAR")))))
 
 (deftest param-dollar-1
-  (is (= '[(shell "echo" (nth *command-line-args* 0))]
-         (x/ast->forms (x/bash->ast "echo $1")))))
+  (is (= '[(shell "cmd" (nth *command-line-args* 0))]
+         (x/ast->forms (x/bash->ast "cmd $1")))))
 
 (deftest param-dollar-2
-  (is (= '[(shell "echo" (nth *command-line-args* 1))]
-         (x/ast->forms (x/bash->ast "echo $2")))))
+  (is (= '[(shell "cmd" (nth *command-line-args* 1))]
+         (x/ast->forms (x/bash->ast "cmd $2")))))
 
 (deftest param-dollar-hash
-  (is (= '[(shell "echo" (dec (count *command-line-args*)))]
-         (x/ast->forms (x/bash->ast "echo $#")))))
+  (is (= '[(shell "cmd" (dec (count *command-line-args*)))]
+         (x/ast->forms (x/bash->ast "cmd $#")))))
 
 #_(deftest param-dollar-at
-    (is (= '[(apply shell "echo" *command-line-args*)]
-           (x/ast->forms (x/bash->ast "echo $@")))))
+    (is (= '[(apply shell "cmd" *command-line-args*)]
+           (x/ast->forms (x/bash->ast "cmd $@")))))
 
 (deftest binary-and
-  (is (= '[(and (zero? (:exit (shell {:continue true} "true"))) (shell "echo" "a"))]
-         (x/ast->forms (x/bash->ast "true && echo a")))))
+  (is (= '[(and (zero? (:exit (shell {:continue true} "true"))) (shell "cmd" "a"))]
+         (x/ast->forms (x/bash->ast "true && cmd a")))))
 
 (deftest binary-or
-  (is (= '[(or (zero? (:exit (shell {:continue true} "true"))) (shell "echo" "a"))]
-         (x/ast->forms (x/bash->ast "true || echo a")))))
+  (is (= '[(or (zero? (:exit (shell {:continue true} "true"))) (shell "cmd" "a"))]
+         (x/ast->forms (x/bash->ast "true || cmd a")))))
 
 (deftest binary-and-3
   (is (= '[(and (and (zero? (:exit (shell {:continue true} "a"))) (shell "b")) (shell "c"))]
@@ -126,31 +126,31 @@
 
 (deftest conditional
   (is (= '[(if (zero? (:exit (shell {:continue true} "true")))
-             (shell "echo" "a")
-             (shell "echo" "b"))]
-         (x/ast->forms (x/bash->ast "if true; then echo a; else echo b; fi")))))
+             (shell "cmd" "a")
+             (shell "cmd" "b"))]
+         (x/ast->forms (x/bash->ast "if true; then cmd a; else cmd b; fi")))))
 
 (deftest conditional-multiple-stmts
   (is (= '[(if (zero? (:exit (shell {:continue true} "true")))
              (do
-               (shell "echo" "a")
-               (shell "echo" "b"))
+               (shell "cmd" "a")
+               (shell "cmd" "b"))
              (do
-               (shell "echo" "c")
-               (shell "echo" "d")))]
-         (x/ast->forms (x/bash->ast "if true; then echo a; echo b; else echo c; echo d; fi")))))
+               (shell "cmd" "c")
+               (shell "cmd" "d")))]
+         (x/ast->forms (x/bash->ast "if true; then cmd a; cmd b; else cmd c; cmd d; fi")))))
 
 (deftest conditional-no-else
   (is (= '[(when (zero? (:exit (shell {:continue true} "true")))
-             (shell "echo" "a"))]
-         (x/ast->forms (x/bash->ast "if true; then echo a; fi")))))
+             (shell "cmd" "a"))]
+         (x/ast->forms (x/bash->ast "if true; then cmd a; fi")))))
 
 (deftest conditional-no-else-multiple-stmts
   (is (= '[(when (zero? (:exit (shell {:continue true} "true")))
              (do
-               (shell "echo" "a")
-               (shell "echo" "b")))]
-         (x/ast->forms (x/bash->ast "if true; then echo a; echo b; fi")))))
+               (shell "cmd" "a")
+               (shell "cmd" "b")))]
+         (x/ast->forms (x/bash->ast "if true; then cmd a; cmd b; fi")))))
 
 (deftest conditional-expr-==
   (is (= ['(= "x" "x")] (x/ast->forms (x/bash->ast "[[ x == x ]]")))))
@@ -162,18 +162,18 @@
   (is (= ['(not= "x" "x")] (x/ast->forms (x/bash->ast "[[ x != x ]]")))))
 
 (deftest conditional-expr-and
-  (is (= '[(and (= "x" "x") (shell "echo" "a"))] (x/ast->forms (x/bash->ast "[[ x == x ]] && echo a")))))
+  (is (= '[(and (= "x" "x") (shell "cmd" "a"))] (x/ast->forms (x/bash->ast "[[ x == x ]] && cmd a")))))
 
 (deftest var-assignment
   (is (= '[(def var "a")] (x/ast->forms (x/bash->ast "var=a")))))
 
 (deftest var-expansion
-  (is (= '[(def var "a") (shell "echo" var)]
-         (x/ast->forms (x/bash->ast "var=a; echo $var")))))
+  (is (= '[(def var "a") (shell "cmd" var)]
+         (x/ast->forms (x/bash->ast "var=a; cmd $var")))))
 
 (deftest envvar
-  (is (= '[(shell {:extra-env {"ENVVAR" "a"}} "bash" "-c" "echo $ENVVAR")]
-         (x/ast->forms (x/bash->ast "ENVVAR=a bash -c 'echo $ENVVAR'")))))
+  (is (= '[(shell {:extra-env {"ENVVAR" "a"}} "bash" "-c" "cmd $ENVVAR")]
+         (x/ast->forms (x/bash->ast "ENVVAR=a bash -c 'cmd $ENVVAR'")))))
 
 (deftest export-var
   (is (= '[(def VAR "a") (alter-var-root (var babashka.process/*defaults*) (fn [m] (update m :extra-env assoc "VAR" VAR)))] (x/ast->forms (x/bash->ast "VAR=a; export VAR")))))
@@ -186,12 +186,12 @@
          (x/ast->forms (x/bash->ast "exit 0")))))
 
 (deftest block
-  (is (= ['(do (shell "echo" "one") (shell "echo" "two"))]
-         (x/ast->forms (x/bash->ast "{ echo one; echo two; }")))))
+  (is (= ['(do (shell "cmd" "one") (shell "cmd" "two"))]
+         (x/ast->forms (x/bash->ast "{ cmd one; cmd two; }")))))
 
 (deftest block-boolean
-  (is (= ['(or (do (zero? (:exit (shell {:continue true} "false")))) (shell "echo" "a"))]
-         (x/ast->forms (x/bash->ast "{ false; } || echo a")))))
+  (is (= ['(or (do (zero? (:exit (shell {:continue true} "false")))) (shell "cmd" "a"))]
+         (x/ast->forms (x/bash->ast "{ false; } || cmd a")))))
 
 (deftest set-builtin
   (is (= []
@@ -203,7 +203,7 @@
 
 #_(deftest var-default
     (is (= [:???]
-           (x/ast->forms (x/bash->ast "echo ${1-mydefault}")))))
+           (x/ast->forms (x/bash->ast "cmd ${1-mydefault}")))))
 
 ;; ----------------------
 
@@ -211,7 +211,7 @@
   (is (map? (second (x/ast->forms+state (x/bash->ast ""))))))
 
 (deftest var-remembered-in-state
-  (is (= {:vars #{'VAR}} (second (x/ast->forms+state (x/bash->ast "echo $VAR"))))))
+  (is (= {:vars #{'VAR}} (second (x/ast->forms+state (x/bash->ast "cmd $VAR"))))))
 
 (deftest declarations-none
   (is (= [] (x/declarations {}))))
@@ -220,12 +220,12 @@
   (is (= '[(def VAR (System/getenv "VAR"))] (x/declarations {:vars #{'VAR}}))))
 
 (deftest bash->bb
-  (is (= (str x/shebang "(require (quote [babashka.process :refer [shell pipeline pb]]))\n(shell \"echo\" \"a\")\n")
-         (x/bash->bb "echo a"))))
+  (is (= (str x/shebang "(require (quote [babashka.process :refer [shell pipeline pb]]))\n(shell \"cmd\" \"a\")\n")
+         (x/bash->bb "cmd a"))))
 
 (deftest bash->bb-var
-  (is (= (str x/shebang "(require (quote [babashka.process :refer [shell pipeline pb]]))\n(def VAR (System/getenv \"VAR\"))\n(shell \"echo\" VAR)\n")
-         (x/bash->bb "echo $VAR"))))
+  (is (= (str x/shebang "(require (quote [babashka.process :refer [shell pipeline pb]]))\n(def VAR (System/getenv \"VAR\"))\n(shell \"cmd\" VAR)\n")
+         (x/bash->bb "cmd $VAR"))))
 
 ;; TODO:
 ;;
